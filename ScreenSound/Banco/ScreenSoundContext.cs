55f95ca7cbb937f6d7f6285cf4f5d67e123341fa
viniciuskirsten <vinicius.k.mendonca@outlook.com>
2024-10-14 08:01:44 -0300

    Instalando e configurando a biblioteca Entity Framework

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -1,4 +1,5 @@
-﻿using ScreenSound.Modelos;
+﻿using Microsoft.EntityFrameworkCore;//para utilizar o Entity Framework
+using ScreenSound.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -6,18 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//ORM - Object-Relational Mapping
+//Responsável por fazer o mapeamento do banco relacional com uma aplicação orientada a objetos.
 
 //Precisamos ter essa classe devido ao Entity utilizar "Context" para o acesso
 namespace ScreenSound.Banco
 {
     //OBS: Não consegui instalar o Entity Framework
-    internal class ScreenSoundContext: dbContext
+    internal class ScreenSoundContext: DbContext
     {
         //String de conexão, coletada nas propriedades do banco no campo "Cadeia de conexão"
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;TrustServerCertificate=False; ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
+        //basicamente ele faz a mesma coisa que o obter conexão, porém utilizando o entity e o SQLServer
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        /*
+         * MÉTODO ANTIGO DE FAZER A CONEXÃO 
         public SqlConnection ObterConexao()
         {
             return new SqlConnection(connectionString);
         }
+        */
     }
 }
