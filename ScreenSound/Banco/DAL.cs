b40a9066200180c1c1e51812aa5074e9ee8a2f54
viniciuskirsten <vinicius.k.mendonca@outlook.com>
2024-10-17 07:58:26 -0300

    Refatorando o código e utilizando alguns tipos mais genericos

--- ScreenSound/Banco/DAL.cs
+++ ScreenSound/Banco/DAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScreenSound.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,26 @@ using System.Threading.Tasks;
 namespace ScreenSound.Banco
 {
     //Abstract pois eu não vou utilizar nenhum obejto dentro dessa classe
-    internal abstract class DAL
-    {
+    internal abstract class DAL <T> where T : class
+    {   //Utilizamos o T para determinar um "tipo genérico"
+        //"where T : class" assim estamos dizendo que este "T"  vai ser uma classe dentro da nossa aplicação
+        private readonly ScreenSoundContext context;
 
+        protected DAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public  IEnumerable<T> Listar()
+        {
+            return context.Set<T>().ToList();//"Set<T>()" é um recurso do Entity Framework que ajuda a pegar qual o tipo que estamos utilizando
+        }
+        public void Adicionar(T objeto) 
+        {
+            context.Set<T>().Add(objeto);
+            context.SaveChanges();
+        }
+        public abstract void Atualizar(T objeto);
+        public abstract void Deletar(T objeto);
     }
 }
--- ScreenSound/Banco/MusicaDAL.cs
+++ ScreenSound/Banco/MusicaDAL.cs
@@ -8,47 +8,32 @@ using System.Threading.Tasks;
 namespace ScreenSound.Banco
 {
     //"MusicaDAL" herda da classe "DAL", que é um genérico.
-    internal class MusicaDAL : DAL
+    internal class MusicaDAL : DAL <Musica> // "Musica" se refere ao campo T que é um tipo genérico no meu DAL.
     {
-        private readonly ScreenSoundContext context;
-
-        public MusicaDAL(ScreenSoundContext context)
+        public MusicaDAL(ScreenSoundContext context) : base(context) { } //"base(contex)" representa que ele vai pegar a informação da nossa classe DAL
+       
+        public  IEnumerable<Musica> Listar()
         {
-            this.context = context;
-        }
-
-        public IEnumerable<Musica> Listar()
-        {
-
             return context.Musicas.ToList();
-
         }
 
-        public void Adicionar(Musica musica)
-        {
-            context.Musicas.Add(musica);
-            context.SaveChanges();
+        public void Adicionar(Musica musica){}
 
-        }
-
-        public void Atualizar(Musica musica)
+        public override void Atualizar(Musica musica)
         {
             context.Musicas.Update(musica);
             context.SaveChanges();
-
         }
 
-        public void Deletar(Musica musica)
+        public override void Deletar(Musica musica)
         {
             context.Musicas.Remove(musica);
             context.SaveChanges();
-
         }
 
         public Musica? RecuperarPeloNome(string nome)
         {
             return context.Musicas.FirstOrDefault(a => a.Nome.Equals(nome));
-
         }
     }
 }
